b9aa1fc02c7bf7a7c15da00f67055bab647087fb
Hyunjin <78681079+Hyunjin21@users.noreply.github.com>
2025-03-04 23:32:08 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,14 +1,15 @@
-a = []
+arr = []
 for i in range(9):
-    a.append(int(input()))
-a.sort()
-b = sum(a)
+    arr.append(int(input()))
+arr.sort()
+total = sum(arr)
 for i in range(9):
-    for j in range(9):
-        if i!=j:
-            if (b-a[i]-a[j])==100:
-                for k in range(9):
-                    if k!=i and k!=j:
-                        print(a[k])
-                exit()
-      
+    for j in range(i+1,9):
+        if (total-arr[i]-arr[j])==100:
+            a = arr[i]
+            b = arr[j]
+            break
+arr.remove(a)
+arr.remove(b)
+for k in arr:
+    print(k)
