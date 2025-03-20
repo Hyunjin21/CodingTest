9fab3cb8708131ce50df3784057d6e042a7a0e2c
Hyunjin <78681079+Hyunjin21@users.noreply.github.com>
2025-03-21 00:31:17 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
@@ -1,7 +1,7 @@
 function solution(a, b) {
-    const str1 = String(a)+String(b);
-    const str2 = String(b)+String(a);
-    answer = str1 >= str2 ? Number(str1) : Number(str2);
+    var str1 = String(a)+String(b);
+    var str2 = String(b)+String(a);
+    var answer = str1 >= str2 ? Number(str1) : Number(str2);
 
     return answer;
 }
