301662e2aaef29ad150c376e33aee4d35f78938d
Hyunjin <78681079+Hyunjin21@users.noreply.github.com>
2025-03-25 00:04:22 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 4.02 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
@@ -1,7 +1,8 @@
 function solution(arr, queries) {
     for (let i = 0; i<queries.length; i++){
-        const [s, e, k] = queries[i];
-        
+        var s = queries[i][0];
+        var e = queries[i][1];
+        var k = queries[i][2];
         for (let j = s; j <= e; j++){
             if(j % k == 0){
                 arr[j] += 1
