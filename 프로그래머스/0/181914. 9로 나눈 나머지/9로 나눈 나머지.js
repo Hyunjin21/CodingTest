ed3913b1af8b7247b4b2ecc3c9816689928a2034
Hyunjin <78681079+Hyunjin21@users.noreply.github.com>
2025-03-28 16:47:46 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 4.72 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.js
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.js
@@ -1,12 +1,8 @@
 function solution(number) {
     var answer = 0;
-    var letter = [];
-    for (let i = 0; i < number.length; i++){
-        letter.push(number[i]);
-    }
     var sum = 0;
-    for (let j = 0; j < letter.length; j++){
-        sum += Number(letter[j]);
+    for (let i = 0; i < number.length; i++){
+        sum += Number(number[i]);
     }
     answer = sum % 9;
     return answer;
