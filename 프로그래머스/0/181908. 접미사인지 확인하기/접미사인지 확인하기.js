bf8b5adc4de5f7ba731465d33358003225bac811
Hyunjin <78681079+Hyunjin21@users.noreply.github.com>
2025-03-29 15:33:56 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -3,9 +3,5 @@ function solution(my_string, is_suffix) {
     for(let i = 0; i < my_string.length; i++){
         arr.push(my_string.slice(i));
     }
-    if (arr.includes(is_suffix) === true){
-        return 1;
-    } else {
-        return 0;
-    }
+    return arr.includes(is_suffix) === true ? 1 : 0;
 }
