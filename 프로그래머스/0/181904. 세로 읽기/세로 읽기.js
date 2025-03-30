bedd66239a77898f418f5b8262ede6a27750fb8c
Hyunjin <78681079+Hyunjin21@users.noreply.github.com>
2025-03-30 13:56:45 +0900

    [level 0] Title: 세로 읽기, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.js
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.js
@@ -1,12 +1,15 @@
 function solution(my_string, m, c) {
     var answer = '';
-    var arr = [];
-    for (let i = 0; i < my_string.length;){
-        arr.push(my_string.slice(i, m+i));
-        i += m;
+    for (let i = c-1; i < my_string.length; i+=m){
+        answer += my_string[i];
     }
-    for (let row of arr){
-        answer += row[c-1];
-    }
+    // var arr = [];
+    // for (let i = 0; i < my_string.length;){
+    //     arr.push(my_string.slice(i, m+i));
+    //     i += m;
+    // }
+    // for (let row of arr){
+    //     answer += row[c-1];
+    // }
     return answer;
 }
