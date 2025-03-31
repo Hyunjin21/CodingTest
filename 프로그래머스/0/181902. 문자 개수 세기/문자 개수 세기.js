15a21aff6ea0c4916ca989df3f7b3b6b4a99678b
Hyunjin <78681079+Hyunjin21@users.noreply.github.com>
2025-03-31 20:55:41 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.37 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.js
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.js
@@ -1,5 +1,5 @@
 function solution(my_string) {
-    const answer = Array(52).fill(0);
+    const answer = Array(52).fill(0); // A-Z: 0~25, a-z: 26~51
     const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
     for (let i = 0; i < my_string.length; i++) {
         const letter = alphabet.indexOf(my_string[i]);
