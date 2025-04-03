d266d08d21bec94f8b7d4b4e1e2dff917174b384
Hyunjin <78681079+Hyunjin21@users.noreply.github.com>
2025-04-03 20:26:12 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.js
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.js
@@ -1,8 +1,16 @@
 function solution(num_list, n) {
     var answer = [];
-    var after = num_list.slice(n);
-    var before = num_list.slice(0, n);
-    answer.push(after);
-    answer.push(before);
-    return answer.flat();
+    // var after = num_list.slice(n);
+    // var before = num_list.slice(0, n);
+    // answer.push(after);
+    // answer.push(before);
+    // return answer.flat();
+    
+    for(let i=n; i<num_list.length; i++){
+        answer.push(num_list[i])
+    }
+    for(let i=0; i<n; i++){
+        answer.push(num_list[i])
+    }
+    return answer;
 }
