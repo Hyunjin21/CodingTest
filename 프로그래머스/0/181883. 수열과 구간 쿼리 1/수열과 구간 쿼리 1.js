4c0fe897f47d094fecdf5188bef190bdecb6ff23
Hyunjin <78681079+Hyunjin21@users.noreply.github.com>
2025-04-06 20:48:58 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 3.80 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
@@ -1,7 +1,7 @@
 function solution(arr, queries) {
     for (let i = 0; i < queries.length; i++){
         var [start, end] = queries[i];
-        for (let j = start; j < end+1; j++){
+        for (let j = start; j <= end; j++){
             arr[j] += 1;
         }
     }
