19df339df62dca632bac2879e1e9745d3a2ba39a
Hyunjin <78681079+Hyunjin21@users.noreply.github.com>
2025-04-10 02:21:21 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
@@ -1,15 +1,17 @@
 function solution(myString, pat) {
-    var str = '';
-    for(let i = 0; i<myString.length; i++){
-        if (myString[i] == 'A'){
-            str += 'B';
-        } else {
-            str += 'A';
-        }
-    }
-    if (str.includes(pat)){
-        return 1;
-    } else {
-        return 0;
-    }
+    var str = myString.replaceAll('A', 'X').replaceAll('B', 'A').replaceAll('X', 'B');
+    return str.includes(pat) ? 1:0;
+    // var str = '';
+    // for(let i = 0; i<myString.length; i++){
+    //     if (myString[i] == 'A'){
+    //         str += 'B';
+    //     } else {
+    //         str += 'A';
+    //     }
+    // }
+    // if (str.includes(pat)){
+    //     return 1;
+    // } else {
+    //     return 0;
+    // }
 }
