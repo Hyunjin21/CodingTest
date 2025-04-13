83cca6687981992d5a07765f891a6e95bfb4a904
Hyunjin <78681079+Hyunjin21@users.noreply.github.com>
2025-04-14 01:42:20 +0900

    [level 0] Title: 문자열 묶기, Time: 35.26 ms, Memory: 50.5 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.js
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.js
@@ -1,16 +1,35 @@
 function solution(strArr) {
     var answer = 0;
-    var count = [];
-    for(let i = 0; i<strArr.length; i++){
-        var num = strArr[i].length;
-        if (count[num] == undefined){
-            count[num] = 1;
-        } else {
-            count[num]++;
+    var max = 0;
+    for(let i=0; i<strArr.length; i++){
+        if(strArr[i].length > max){
+            max = strArr[i].length;   
         }
-        if(count[num] > answer){
-            answer = count[num];
+    }
+    for(let j=1; j<=max; j++){
+        var count = 0;
+        for(let k=0; k<strArr.length; k++){
+            if(strArr[k].length === j){
+                count++;
+            }
+        }
+        if (count > answer){
+            answer = count;
         }
     }
     return answer;
+    // var answer = 0;
+    // var count = [];
+    // for(let i = 0; i<strArr.length; i++){
+    //     var num = strArr[i].length;
+    //     if (count[num] == undefined){
+    //         count[num] = 1;
+    //     } else {
+    //         count[num]++;
+    //     }
+    //     if(count[num] > answer){
+    //         answer = count[num];
+    //     }
+    // }
+    // return answer;
 }
