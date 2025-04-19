8c45c1cf7534129c3e3c7e96f957b41174049b87
Hyunjin <78681079+Hyunjin21@users.noreply.github.com>
2025-04-20 01:12:49 +0900

    [level 0] Title: 두 수의 합, Time: 36.32 ms, Memory: 39.7 MB -BaekjoonHub

--- 프로그래머스/0/181846. 두 수의 합/두 수의 합.js
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.js
@@ -1,5 +1,24 @@
 function solution(a, b) {
-//     var answer = '';
-    return (BigInt(a) + BigInt(b)).toString();
-//     return answer;
+    var answer = '';
+    var carry = 0; // 올림수
+    
+    let i = a.length - 1;
+    let j = b.length - 1;
+    
+    // 뒤에서부터 하나씩 더함
+    while (i >= 0 || j >= 0 || carry > 0) {
+        let digitA = i >= 0 ? Number(a[i]) : 0;
+        let digitB = j >= 0 ? Number(b[j]) : 0;
+
+        let sum = digitA + digitB + carry;
+
+        answer = (sum % 10) + answer; // 현재 자리 숫자
+        carry = Math.floor(sum / 10); // 올림 처리
+
+        i--;
+        j--;
+    }
+
+    return answer;
+    // return (BigInt(a) + BigInt(b)).toString();
 }
