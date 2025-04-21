fa5a5458cd9a545912628acf29c26735cf05f43e
Hyunjin <78681079+Hyunjin21@users.noreply.github.com>
2025-04-22 01:51:09 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
@@ -1,13 +1,23 @@
 function solution(date1, date2) {
-    var answer = 0;
-    for(let i = 0; i<date1.length; i++){
-        if (date1[i] < date2[i]){
-            answer = 1;
-            break;
-        } else if (date1[i] > date2[i]){
-            answer = 0;
-            break;
+    const [y1, m1, d1] = date1;
+    const [y2, m2, d2] = date2;
+    if (y1 < y2) return 1;
+    else if (y1 == y2) {
+        if (m1 < m2) return 1;
+        else if (m1 == m2) {
+            if (d1 < d2) return 1;
         }
     }
-    return answer;
+    return 0;
+    // var answer = 0;
+    // for(let i = 0; i<date1.length; i++){
+    //     if (date1[i] < date2[i]){
+    //         answer = 1;
+    //         break;
+    //     } else if (date1[i] > date2[i]){
+    //         answer = 0;
+    //         break;
+    //     }
+    // }
+    // return answer;
 }
