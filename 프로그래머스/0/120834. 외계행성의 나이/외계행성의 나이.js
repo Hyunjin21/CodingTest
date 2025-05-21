ea7ad355670d286e5ed94cf658a4dd06e4243c63
Hyunjin <78681079+Hyunjin21@users.noreply.github.com>
2025-05-21 23:34:20 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,10 +1,16 @@
 function solution(age) {
-    var answer = '';
     const ageProgram = ['a','b','c','d','e','f','g','h','i','j'];
-    const strAge = String(age);
-    for(let i=0; i<strAge.length; i++){
-        const index = parseInt(strAge[i]);
-        answer += ageProgram[index];
-    }
-    return answer;
+    return String(age)
+        .split('')
+        .map(index => ageProgram[parseInt(index)])
+        .join('');
+    
+    // var answer = '';
+    // const ageProgram = ['a','b','c','d','e','f','g','h','i','j'];
+    // const strAge = String(age);
+    // for(let i=0; i<strAge.length; i++){
+    //     const index = parseInt(strAge[i]);
+    //     answer += ageProgram[index];
+    // }
+    // return answer;
 }
