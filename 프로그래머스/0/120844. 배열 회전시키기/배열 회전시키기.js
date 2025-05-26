a55acaf9ec9a20337dd1155bba0c0599edb1a154
Hyunjin <78681079+Hyunjin21@users.noreply.github.com>
2025-05-27 02:52:01 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -1,17 +1,25 @@
 function solution(numbers, direction) {
-    var answer = [];
-    if(direction == 'right'){
-        answer.push(numbers[numbers.length-1]);
-    } 
-    for(let i=0; i<numbers.length-1; i++){
-        if(direction == 'right'){
-            answer.push(numbers[i]);
-        } else if(direction == 'left'){
-            answer.push(numbers[i+1]);
-        }
+    if(direction === "right"){
+        numbers.unshift(numbers.pop());
+    }else{
+        numbers.push(numbers.shift())
     }
-    if (direction == 'left'){
-        answer.push(numbers[0]);
-    }
-    return answer;
+    return numbers;
 }
+// function solution(numbers, direction) {
+//     var answer = [];
+//     if(direction == 'right'){
+//         answer.push(numbers[numbers.length-1]);
+//     } 
+//     for(let i=0; i<numbers.length-1; i++){
+//         if(direction == 'right'){
+//             answer.push(numbers[i]);
+//         } else if(direction == 'left'){
+//             answer.push(numbers[i+1]);
+//         }
+//     }
+//     if (direction == 'left'){
+//         answer.push(numbers[0]);
+//     }
+//     return answer;
+// }
