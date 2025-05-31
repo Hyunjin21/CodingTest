13367b080d351db0e392b9f21c9112994c868a05
Hyunjin <78681079+Hyunjin21@users.noreply.github.com>
2025-06-01 00:20:19 +0900

    [level 0] Title: 합성수 찾기, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.js
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.js
@@ -1,6 +1,6 @@
 function solution(n) {
     var answer = 0;
-    for (let i = 4; i <= n; i++){
+    for (let i = 2; i <= n; i++){
         let count = 0;
         for (let j = 0; j<=i; j++){
             if(i%j == 0) {
