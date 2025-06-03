dcfe9bd7b27a4bb05b5650af3a380676a607adab
Hyunjin <78681079+Hyunjin21@users.noreply.github.com>
2025-06-03 19:32:09 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -1,5 +1,5 @@
 function solution(my_string) {
-    var str = my_string.replace(/[a-z]/g,'').split('').map(Number);
+    var str = my_string.replace(/[a-zA-Z]/g,'').split('').map(Number);
     str.sort((a, b) => a - b);
     return str;
 }
