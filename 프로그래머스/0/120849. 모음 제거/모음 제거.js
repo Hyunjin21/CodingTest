bd1e1271ed429aec5750890d5328ff05937fa1e5
Hyunjin <78681079+Hyunjin21@users.noreply.github.com>
2025-06-03 19:21:37 +0900

    [level 0] Title: 모음 제거, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.js
@@ -2,6 +2,6 @@ function solution(my_string) {
     var answer = '';
     let str = my_string.split('');
     let arr = ['a', 'e', 'i', 'o', 'u'];
-    answer = str.filter((char)=> !arr.includes(char)).join('');
+    answer = str.filter(char => !arr.includes(char)).join('');
     return answer;
 }
