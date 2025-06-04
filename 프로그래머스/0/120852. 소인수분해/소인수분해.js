9bb826bbd4562cec5c811036dc58a36d6feec36c
Hyunjin <78681079+Hyunjin21@users.noreply.github.com>
2025-06-04 22:56:16 +0900

    [level 0] Title: 소인수분해, Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    var answer = [];
+    for(let i = 2; i<=n; i++){
+        while (n % i == 0){
+            answer.push(i);
+            n = n / i;
+        }
+    }
+    
+    return [...new Set(answer)];
+}
