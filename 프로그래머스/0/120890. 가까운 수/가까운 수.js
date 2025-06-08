64dd792b9d24c3b18dbe0854faa4bec1f4d98b22
Hyunjin <78681079+Hyunjin21@users.noreply.github.com>
2025-06-08 17:06:53 +0900

    [level 0] Title: 가까운 수, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.js
@@ -0,0 +1,13 @@
+function solution(array, n) {
+    let new_arr = [];
+    array.sort((a, b) => a - b);
+    for(let i=0; i<array.length; i++){
+        new_arr.push(Math.abs(array[i]-n))
+    }
+    let idx = new_arr.indexOf(Math.min(...new_arr));
+    if (idx.length == 1){
+        return array[idx];
+    } else {
+        return Math.min(array[idx]);
+    }
+}
