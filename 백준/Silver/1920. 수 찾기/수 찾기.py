4f1eb1b1bba33cd167a26f8dbbfcac8fcb51547b
Hyunjin <78681079+Hyunjin21@users.noreply.github.com>
2025-06-09 21:40:52 +0900

    [Silver IV] Title: 수 찾기, Time: 228 ms, Memory: 50700 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,26 +1,13 @@
+import bisect
 N = int(input())
 A = list(map(int, input().split()))
 A.sort()
 M = int(input())
-M_list = list(map(int,input().split()))
+M_list = list(map(int, input().split()))
 
-def binary_search(target, data):
-    start = 0
-    end = len(data) - 1
-
-    while start <= end:
-        mid = (start+end)//2
-
-        if data[mid] == target:
-            return 1
-        elif data[mid] > target:
-            end = mid - 1
-        else:
-            start = mid + 1
-    return 0
-
-for i in M_list:
-    if binary_search(i, A):
+for target in M_list:
+    idx = bisect.bisect_left(A, target)
+    if idx < N and A[idx] == target:
         print(1)
     else:
         print(0)
