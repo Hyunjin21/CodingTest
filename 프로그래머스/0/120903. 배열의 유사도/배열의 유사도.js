732b676e5d093f6d9fa2730d140ca31af8bf2fa4
Hyunjin <78681079+Hyunjin21@users.noreply.github.com>
2025-06-10 14:31:52 +0900

    [level 0] Title: 배열의 유사도, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,13 +1,20 @@
 function solution(s1, s2) {
-    var answer = 0;
-    s1.sort();
-    s2.sort();
-    for(let i = 0; i<s1.length; i++){
-        for(let j = 0; j<s2.length; j++){
-            if(s1[i] === s2[j]){
-                answer += 1;
-            }
+    let count = 0;
+    for(let i of s1){
+        if(s2.includes(i)){
+            count++;
         }
     }
-    return answer;
+    return count;
+    // var answer = 0;
+    // s1.sort();
+    // s2.sort();
+    // for(let i = 0; i<s1.length; i++){
+    //     for(let j = 0; j<s2.length; j++){
+    //         if(s1[i] === s2[j]){
+    //             answer += 1;
+    //         }
+    //     }
+    // }
+    // return answer;
 }
