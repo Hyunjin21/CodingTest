1387333a829f735c11153d3ea22d073b23ed5d77
Hyunjin <78681079+Hyunjin21@users.noreply.github.com>
2025-06-12 22:29:46 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120860. 직사각형 넓이 구하기/README.md
+++ 프로그래머스/0/120860. 직사각형 넓이 구하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 직사각형 넓이 구하기 - 120860 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120860) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120860#) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.06 ms
+메모리: 33.5 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 12일 22:27:44
+2025년 06월 12일 22:29:44
 
 ### 문제 설명
 
--- 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
+++ 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
@@ -1,21 +1,29 @@
 function solution(dots) {
-    let width = 0;
-    let height = 0;
-    if (dots[0][0] != dots[1][0]){
-        width = dots[1][0] - dots[0][0];
-    } else if (dots[0][0] != dots[2][0]){
-        width = dots[2][0] - dots[0][0];
-    } else if (dots[0][0] != dots[3][0]){
-        width = dots[3][0] - dots[0][0];
+    let x = [];
+    let y = [];
+    for (let pos of dots) {
+        x.push(pos[0]);
+        y.push(pos[1]);
     }
+    return (Math.max(...x) - Math.min(...x)) * (Math.max(...y) - Math.min(...y));
     
-    if (dots[0][1] != dots[1][1]){
-        height = dots[1][1] - dots[0][1];
-    } else if (dots[0][1] != dots[2][1]){
-        height = dots[2][1] - dots[0][1];
-    } else if (dots[0][1] != dots[3][1]){
-        height = dots[3][1] - dots[0][1];
-    } 
-    let answer = Math.abs(width) * Math.abs(height);
-    return answer;
+//     let width = 0;
+//     let height = 0;
+//     if (dots[0][0] != dots[1][0]){
+//         width = dots[1][0] - dots[0][0];
+//     } else if (dots[0][0] != dots[2][0]){
+//         width = dots[2][0] - dots[0][0];
+//     } else if (dots[0][0] != dots[3][0]){
+//         width = dots[3][0] - dots[0][0];
+//     }
+    
+//     if (dots[0][1] != dots[1][1]){
+//         height = dots[1][1] - dots[0][1];
+//     } else if (dots[0][1] != dots[2][1]){
+//         height = dots[2][1] - dots[0][1];
+//     } else if (dots[0][1] != dots[3][1]){
+//         height = dots[3][1] - dots[0][1];
+//     } 
+//     let answer = Math.abs(width) * Math.abs(height);
+//     return answer;
 }
