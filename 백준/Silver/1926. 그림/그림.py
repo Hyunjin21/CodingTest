d6b6f24b52956e1372b15a7b7ebb6b04e053e2b5
Hyunjin <78681079+Hyunjin21@users.noreply.github.com>
2025-06-17 18:07:42 +0900

    [Silver I] Title: 그림, Time: 296 ms, Memory: 34976 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -1,20 +1,23 @@
+from collections import deque
+
 n, m = map(int,input().split())
 paint = [list(map(int,input().split())) for _ in range(n)]
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-def dfs_stack(x, y):
-    stack = [(x, y)]
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
     paint[y][x] = 0
     area = 1
-    while stack:
-        cx, cy = stack.pop()
+    while queue:
+        cx, cy = queue.popleft()
         for i in range(4):
             nx = cx + dx[i]
             ny = cy + dy[i]
             if 0 <= nx < m and 0 <= ny < n and paint[ny][nx] == 1:
                 paint[ny][nx] = 0
-                stack.append((nx,ny))
+                queue.append((nx,ny))
                 area += 1
     return area
 
@@ -24,6 +27,6 @@ for y in range(n):
     for x in range(m):
         if paint[y][x] == 1:
             cnt += 1
-            max_area = max(max_area, dfs_stack(x, y))
+            max_area = max(max_area, bfs(x, y))
 print(cnt)
 print(max_area)
