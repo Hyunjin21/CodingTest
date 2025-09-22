8a19aede5f3168d17a692b2089b456153e91f40c
Hyunjin Yoo <78681079+Hyunjin21@users.noreply.github.com>
2025-09-22 13:22:26 +0900

    [Gold I] Title: 구간 곱 구하기, Time: 1184 ms, Memory: 136424 KB -BaekjoonHub

--- 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.py
+++ 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.py
@@ -1,45 +1,45 @@
 import sys
 input = sys.stdin.readline
 
-def merge(x,y):
-    return x * y
-
-def init(node,left,right):
-    if left == right:
-        tree[node] = arr[left]
-        return tree[node]
-    mid = (left+right)//2
-    tree[node] = merge(init(node*2,left,mid),init(node*2+1,mid+1,right)) % 1000000007
-    return tree[node]
-
-def find(node,left,right,x,y):
-    if x <= left and right <= y:
-        return tree[node]
-    if y < left or x > right:
-        return 1
-    mid = (left + right) // 2
-    return merge(find(node * 2, left, mid, x, y), find(node * 2 + 1, mid + 1, right, x, y)) % 1000000007
-
-def update(node,left,right,x,y):
-    if left == right == x:
-        tree[node] = y
-        return tree[node]
-    if x < left or x > right:
-        return tree[node]
-    mid = (left+right)//2
-    tree[node] = merge(update(node*2,left,mid,x,y), update(node*2+1,mid+1,right,x,y)) % 1000000007
-    return tree[node]
-
-N, M, K = map(int,input().split())
+N, M, K = map(int, input().split())
 arr = [int(input()) for _ in range(N)]
-tree = [1] * (N*4+1)
+tree = [1] * (N * 4)
 
-init(1,0,N-1)
+def merge(x, y):
+    return (x * y) % 1000000007
 
-for _ in range(M+K):
-    a, b, c = map(int,input().split())
+def init(start, end, idx):
+    if start == end:
+        tree[idx] = arr[start]
+        return tree[idx]
+    mid = (start + end) // 2
+    tree[idx] = merge(init(start, mid, idx * 2), init(mid + 1, end, idx * 2 + 1))
+    return tree[idx]
+
+def find(start, end, idx, left, right):
+    if left > end or right < start:
+        return 1
+    if left <= start and end <= right:
+        return tree[idx]
+    mid = (start + end) // 2
+    return merge(find(start, mid, idx * 2, left, right), find(mid + 1, end, idx * 2 + 1, left, right))
+
+def update(start, end, idx, target, new_value):
+    if target < start or target > end:
+        return tree[idx]
+    if start == end:
+        tree[idx] = new_value
+        return tree[idx]
+    mid = (start + end) // 2
+    tree[idx] = merge(update(start, mid, idx * 2, target, new_value), update(mid + 1, end, idx * 2 + 1, target, new_value))
+    return tree[idx]
+
+init(0, N - 1, 1)
+
+for _ in range(M + K):
+    a, b, c = map(int, input().split())
     if a == 1:
-        arr[b-1] = c
-        update(1,0,N-1,b-1,c)
-        continue
-    print(find(1,0,N-1,b-1,c-1))
+        arr[b - 1] = c
+        update(0, N - 1, 1, b - 1, c)
+    else:
+        print(find(0, N - 1, 1, b - 1, c - 1))
