8749c2cfc582504eecc91e9397597c252fa21e20
Hyunjin Yoo <78681079+Hyunjin21@users.noreply.github.com>
2026-05-13 17:08:52 +0900

    [level 0] Title: 분수의 덧셈, Time: 6.79 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -1,7 +1,20 @@
-import math
+# import math
+
+# def solution(numer1, denom1, numer2, denom2):
+#     num = denom1*numer2 + denom2*numer1
+#     denom = denom1*denom2
+#     gcd = math.gcd(num, denom)
+#     return [num//gcd, denom//gcd]
 
 def solution(numer1, denom1, numer2, denom2):
     num = denom1*numer2 + denom2*numer1
     denom = denom1*denom2
-    gcd = math.gcd(num, denom)
-    return [num//gcd, denom//gcd]
+    
+    for i in range(min(num, denom), 1, -1):
+        if num % i == 0 and denom % i == 0:
+            num //= i
+            denom //= i
+            break
+        
+    answer = [num, denom]
+    return answer
