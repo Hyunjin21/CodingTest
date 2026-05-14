81da51df8a04452796b3529bb24e41a119dc4bf8
Hyunjin Yoo <78681079+Hyunjin21@users.noreply.github.com>
2026-05-14 18:18:44 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.01 ms, Memory: 9 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.py
@@ -1,5 +1,12 @@
 def solution(my_string, letter):
-    return my_string.replace(letter, '')
+    answer = ''
+    for i in my_string:
+        if letter != i:
+            answer += i
+    return answer
+    
+    # return my_string.replace(letter, '')
+    
     # answer = []
     # for i in my_string:
     #     if letter != i:
