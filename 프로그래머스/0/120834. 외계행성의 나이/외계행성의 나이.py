1ce463029f41a4fda6b6cda1137866b8f9c3b4ad
Hyunjin Yoo <78681079+Hyunjin21@users.noreply.github.com>
2026-05-14 19:12:23 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.02 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.py
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.py
@@ -1,7 +1,10 @@
 def solution(age):
     answer = ''
     alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
-    age = [int(i) for i in str(age)]
-    for j in age:
-        answer += alpha[j]
+    age = str(age)
+    for i in age:
+        answer += alpha[int(i)]
+    # age = [int(i) for i in str(age)]
+    # for j in age:
+    #     answer += alpha[j]
     return answer
