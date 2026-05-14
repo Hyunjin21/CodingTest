1161246c3b429b7a3ea1748ebfe38bd5385f60e4
Hyunjin Yoo <78681079+Hyunjin21@users.noreply.github.com>
2026-05-14 19:39:54 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.00 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
@@ -1,9 +1,16 @@
 def solution(emergency):
+    arr = sorted(emergency, reverse = True)
     answer = []
     for i in emergency:
-        index = 1
-        for j in emergency:
-            if i < j:
-                index += 1
-        answer.append(index)
+        idx = arr.index(i)+1
+        answer.append(idx)
     return answer
+    
+    # answer = []
+    # for i in emergency:
+    #     index = 1
+    #     for j in emergency:
+    #         if i < j:
+    #             index += 1
+    #     answer.append(index)
+    # return answer
