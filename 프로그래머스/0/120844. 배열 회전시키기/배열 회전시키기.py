738b004baf35cc87be5a00a5e8857fde4568e33a
Hyunjin Yoo <78681079+Hyunjin21@users.noreply.github.com>
2026-05-18 18:05:37 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.00 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -1,12 +1,17 @@
 def solution(numbers, direction):
-    answer = []
-    length = len(numbers)
     if direction == 'right':
-        answer.append(numbers[length-1])
-        for i in range(0, len(numbers)-1):
-            answer.append(numbers[i])
-    elif direction == 'left':
-        for j in range(1, len(numbers)):
-            answer.append(numbers[j])
-        answer.append(numbers[0])
+        answer = [numbers[-1]] + numbers[:len(numbers)-1]
+    else:
+        answer = numbers[1:] + [numbers[0]]
+    
+    # answer = []
+    # length = len(numbers)
+    # if direction == 'right':
+    #     answer.append(numbers[length-1])
+    #     for i in range(0, len(numbers)-1):
+    #         answer.append(numbers[i])
+    # elif direction == 'left':
+    #     for j in range(1, len(numbers)):
+    #         answer.append(numbers[j])
+    #     answer.append(numbers[0])
     return answer
