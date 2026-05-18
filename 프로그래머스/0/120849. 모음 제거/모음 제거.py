c58ce24eb91ade928c579991e5029add8f9d3c2c
Hyunjin Yoo <78681079+Hyunjin21@users.noreply.github.com>
2026-05-18 22:35:28 +0900

    [level 0] Title: 모음 제거, Time: 0.00 ms, Memory: 9.02 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.py
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -1,6 +1,11 @@
 def solution(my_string):
-    answer = ''
-    for i in str(my_string):
-        if i != 'a' and i != 'e' and i != 'i' and i != 'o' and i != 'u':
-            answer += i
-    return answer
+    arr = ['a', 'e', 'i', 'o', 'u']
+    for i in arr:
+        my_string = my_string.replace(i, '')
+    return my_string
+
+    # answer = ''
+    # for i in str(my_string):
+    #     if i != 'a' and i != 'e' and i != 'i' and i != 'o' and i != 'u':
+    #         answer += i
+    # return answer
