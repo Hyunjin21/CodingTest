5506689c1984bd1ec458cc33fe37fc5f17c37b2e
Hyunjin Yoo <78681079+Hyunjin21@users.noreply.github.com>
2026-05-21 17:31:09 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.01 ms, Memory: 8.8 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.py
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.py
@@ -1,3 +1,7 @@
 def solution(my_string):
-    answer = ''.join(dict.fromkeys(my_string))
+    answer = ''
+    for i in my_string:
+        if i not in answer:
+            answer += i
+    # answer = ''.join(dict.fromkeys(my_string))
     return answer
